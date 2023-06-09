f3a1a6183d87dba7293cfaae049fdcfe8aa59b29
sadys castro <sadyscastro@gmail.com>
2023-06-09 16:12:04 -0600

    login consulta de gmail, password

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,21 +3,21 @@ const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt');
 
 
-const crearUsuario = async (req, res= response) => {
+const crearUsuario = async (req, res = response) => {
 
     try {
         const { email, password } = req.body;
 
         //valido si el email ya exist
-        const existeEmail =  await Usuario.findOne({ email });
+        const existeEmail = await Usuario.findOne({ email });
 
-        if(existeEmail){
+        if (existeEmail) {
             return res.status(400).json({
                 ok: false,
                 msg: 'El correo ya esta registrado'
             });
         }
-        
+
         const usuario = new Usuario(req.body);
 
         //Encriptar contraseña
@@ -25,7 +25,7 @@ const crearUsuario = async (req, res= response) => {
         usuario.password = bcrypt.hashSync(password, salt);
         //Guardar registro db
         await usuario.save();
-
+        //crear JWT
         const token = await generarJWT(usuario.id)
 
         res.json({
@@ -33,10 +33,10 @@ const crearUsuario = async (req, res= response) => {
             usuario,
             token
         })
-        
+
     } catch (error) {
         console.log(error);
-        
+
         res.status(500).json({
             ok: false,
             msg: 'Hable con el administrador'
@@ -46,15 +46,48 @@ const crearUsuario = async (req, res= response) => {
 
 
 
-const ingrsoDeUsuario = async (req, res= response) => {
+const ingrsoDeUsuario = async (req, res = response) => {
 
-    console.log(req.body);
+    try {
+        const { email, password } = req.body;
 
-    return res.status(200).json({
-        ok: true,
-        msg: 'exitoso',
-        data: req.body
-    });
+        const usuario = await Usuario.findOne({ email });
+        // console.log(usuario)
+
+        if (!usuario) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'El email no encontrado'
+            });
+        }
+
+        //validar password
+        const validPassword = bcrypt.compareSync(password, usuario.password);
+        // console.log(validPassword)
+
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'La contraseña no es valida'
+            });
+        }
+
+        const token = await generarJWT(usuario.id)
+
+        res.json({
+            ok: true,
+            usuario,
+            token
+        })
+
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+    }
 
 }
 
@@ -62,3 +95,4 @@ module.exports = {
     crearUsuario,
     ingrsoDeUsuario
 }
+
